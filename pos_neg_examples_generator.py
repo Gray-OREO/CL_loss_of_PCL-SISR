2b2b95f68765bff2e6271321c1f02ee5834066b9
Gray Lee <64567170+Gray-OREO@users.noreply.github.com>
2022-12-10 18:22:53 +0800

    Update pos_neg_examples_generator.py

--- pos_neg_examples_generator.py
+++ pos_neg_examples_generator.py
@@ -5,12 +5,16 @@ import numpy as np
 
 
 def get_neg_samples(hr_im, ne_num=4):
-    # img_hr = torch.tensor(hr_im)  # [B,C,H,W]
+    '''
+    Design the number of negative examples by the parameter 'ne_num', and change the operator in line 14-19 for your own strategy.
+    hr_im: [batch_size, channel, height, width] tensor
+    negsamps: [batch_size, ne_num, channel, height, width] tensor
+    '''
     B, C, H, W = hr_im.shape
     negsamps = torch.zeros(B, ne_num, C, H, W)
     for b, img_hr__ in enumerate(hr_im):
         img_hr__ = img_hr__.detach().cpu().numpy()
-        for k in range(ne_num):
+        for k in range(ne_num):  
             min_size = 11
             addition = random.choice((0, 2, 4, 6, 8, 10, 12))
             size = min_size + addition
@@ -22,6 +26,9 @@ def get_neg_samples(hr_im, ne_num=4):
 
 
 def get_pos_samples(img_hr, pe_num=1):
+    '''
+    Similar to get_neg_samples.
+    '''
     B, C, H, W = img_hr.shape
     possamp = torch.zeros(B, pe_num, C, H, W)
     img_hr = img_hr.detach().cpu().numpy()
